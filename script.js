49b4592d28585a16046fd17ad4114b16c8136c70
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-01-12 23:45:54 +0100

    task 4

--- script.js
+++ script.js
@@ -51,3 +51,27 @@ function busStop(people) {
 }
 // console.log(busStop([[15,0],[5,1],[5,1]]))
 // console.log(busStop1([[15,0],[5,1],[5,1]]))
+
+//Task 4
+//I have four positive integers, A, B, C and D, where A < B < C < D.
+// The input is a list of the integers A, B, C, D, AxB, BxC, CxD, DxA in some order.
+// Your task is to return the value of D.
+
+function alphabet(ns) {
+  const sortedArr = ns.sort((a, b) => {
+    return a - b;
+  });
+
+  if (sortedArr[2] === sortedArr[0] * sortedArr[1]) {
+    return sortedArr[7] / sortedArr[3];
+  } else {
+    return sortedArr[7] / sortedArr[2];
+  }
+}
+
+// alphabet([2, 3, 4, 1, 12, 6, 2, 4]);
+// alphabet([2, 6, 7, 3, 14, 35, 15, 5]);
+// alphabet([20, 10, 6, 5, 4, 3, 2, 12]);
+// alphabet([2, 6, 18, 3, 6, 7, 42, 14]);
+// alphabet([7, 96, 8, 240, 12, 140, 20, 56]);
+// alphabet([20, 30, 6, 7, 4, 42, 28, 5]);
