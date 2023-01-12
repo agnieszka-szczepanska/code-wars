350d626e63a84f0dac09af7a669e21a4ec92c029
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-01-12 21:41:56 +0100

    task 2

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ function longest(s1, s2) {
   }
   return array.sort().join("");
 }
+
+// Task 2
+//Given an array of integers, remove the smallest value.
+//Do not mutate the original array/list.
+//If there are multiple elements with the same value, remove the one with a lower index.
+//If you get an empty array/list, return an empty array/list.
+//Don't change the order of the elements that are left.
+
+function removeSmallest(numbers) {
+  if (numbers === []) {
+    return numbers;
+  } else {
+    const a = numbers.indexOf(Math.min(...numbers));
+    let b = numbers.slice(0, a);
+    let c = numbers.slice(a + 1, numbers.length);
+    return b.concat(c);
+  }
+}
+
+// console.log(removeSmallest([1, 2, 3, 4, 5]))
+
+//Input: [1,2,3,4,5], output = [2,3,4,5]
+//Input: [5,3,2,1,4], output = [5,3,2,4]
+//Input: [2,2,1,2,1], output = [2,2,2,1]
