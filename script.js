5add9c9d799203f0b56f89f7a2b0d4d8ddafdefe
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-01-14 23:18:20 +0100

    task 7

--- script.js
+++ script.js
@@ -104,3 +104,34 @@ function removeSmallest(numbers) {
   }
 }
 // console.log(removeSmallest([5, 3, 2, 1, 4]));
+
+//Task 7
+//Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+function longest(s1, s2) {
+  let b = s1 + s2;
+  b = b
+    .split("")
+    .sort()
+    .filter((element, index, array) => {
+      return array.indexOf(element) === index;
+    })
+    .join("");
+
+  return b;
+}
+// Task 7 - in other way
+
+function longest(s1, s2) {
+  const everyLetters = s1.split("").concat(s2.split(""));
+  const array = [];
+  for (const letter of everyLetters) {
+    if (array.indexOf(letter) === -1) {
+      array.push(letter);
+    }
+  }
+  return array.sort().join("");
+}
+
+// console.log(longest("aretheyhere", "yestheyarehere"));
+// console.log(longest("aabbccdd", "aabbccddeehijf"));
