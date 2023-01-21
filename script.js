6ebfd0e1aba2a86cd5e30d666fad6212cf30d4c7
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-01-21 21:45:46 +0100

    task 9

--- script.js
+++ script.js
@@ -144,3 +144,12 @@ function invert(array) {
 }
 
 // console.log(invert([1,2,3,4,5]))
+
+// Task 9
+//  Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
+
+// console.log(repeatStr(3, "*"));
