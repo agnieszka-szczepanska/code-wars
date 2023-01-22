437ec71dbbd44957760bd46be46cd80113a4a688
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-01-22 21:24:28 +0100

    task 11

--- script.js
+++ script.js
@@ -161,3 +161,20 @@ function addLength(str) {
 }
 
 // console.log(addLength("apple ban"));
+
+// task 11
+//Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+function createPhoneNumber(numbers) {
+  const result = [
+    "(",
+    numbers.slice(0, 3).join(""),
+    ")",
+    numbers.slice(3, 6).join(""),
+    "-",
+    numbers.slice(6, 10).join(""),
+  ];
+  return result.join("");
+}
+
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]); // => returns "(123) 456-7890"
