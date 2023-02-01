be2086247b1ff27af0d103970e89f6d804568681
Agnieszka Szczepańska <szczepanska.agnieszka01@gmail.com>
2023-02-01 20:58:33 +0100

    task 15

--- script.js
+++ script.js
@@ -216,3 +216,19 @@ function minSum(arr) {
 }
 
 console.log(minSum([1, 2, 3, 4]));
+
+//task 15
+//Sum Mixed Array
+
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+// Return your answer as a number.
+
+function sumMix(x) {
+  return x.reduce((accumulator, currentValue) => {
+    return accumulator + Number(currentValue);
+  }, 0);
+}
+
+sumMix([9, 3, "7", "3"]);
+// sumMix(['5', '0', 9, 3, 2, 1, '9', 6, 7]);
+// sumMix(['3', 6, 6, 0, '5', 8, 5, '6', 2,'0']);
